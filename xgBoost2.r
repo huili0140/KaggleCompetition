8f6ca809522dbe9e1a118aae16768e6e0864d099
Hui Li <huili.alpine@gmail.com>
2015-11-18 13:17:55 -0800

    xgboost

--- xgBoost2.r
+++ xgBoost2.r
@@ -57,16 +57,16 @@ watchlist <- list(val = dval, train = dtrain)
 set.seed(100)
 param <- list(  objective = "reg:linear", 	# specify the learning task
                 booster = "gbtree",			# select booster from gbtree or gblinear
-                eta = 0.01, 				# control the learning rate, 0.06, 0.02, 0.01,
+                eta = 0.015, 				# control the learning rate, 0.06, 0.02, 0.01,
                 max_depth = 10, 			# maximum depth of the tree
-				min_child_weight = 5, 		# larger to be more conservative
+				        min_child_weight = 5, 		# larger to be more conservative
                 subsample = 0.9, 			# sub-sample ratio of the training instances
                 colsample_bytree = 0.7	 	# sub-sample ratio of the columns			
 )
 
 clf <- xgb.train(	params = param, 
 					data = dtrain, 			# xgb.DMatrix training data
-					nrounds = 3000, 		# changed from 300
+					nrounds = 5000, 		# changed from 300
 					verbose = 0,			# print information of performance
 					early.stop.round = 100,	# stop if performance gets worse after the first n rounds
 					watchlist = watchlist,
@@ -78,4 +78,4 @@ clf <- xgb.train(	params = param,
 pred1 <- exp(predict(clf, data.matrix(test[,feature.names]))) -1
 submission <- data.frame(Id = test$Id, Sales = pred1)
 cat("saving the submission file...\n")
-write.csv(submission, "xgboost.csv", row.names = FALSE)
+write.csv(submission, "xgboost.15.5000.csv", row.names = FALSE)
